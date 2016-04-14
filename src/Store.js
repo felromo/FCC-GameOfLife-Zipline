8d678124cb292805af9079bbf7230e8298eef7a7
Tyrantz <raymanv3@gmail.com>
2016-04-14 04:33:21 +0000

    imports dispatcher creates handleactions and registers it to handler

--- src/Store.js
+++ src/Store.js
@@ -1,4 +1,5 @@
 import {EventEmitter} from 'events';
+import dispatcher from './Dispatcher';
 
 class Store extends EventEmitter {
   constructor() {
@@ -14,8 +15,17 @@ class Store extends EventEmitter {
   getSize() {
     return this.size;
   }
+
+  handleActions(action) {
+    switch(action.type) {
+      case 'CHANGE_SIZE': {
+        this.changeSize(action.size);
+      }
+    }
+  }
 }
 
 const gameOfLifeStore = new Store;
+dispatcher.register(gameOfLifeStore.handleActions.bind(gameOfLifeStore));
 
 export default gameOfLifeStore;
