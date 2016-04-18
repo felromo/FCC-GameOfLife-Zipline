039c75dc8f32fac11200569b84f1d3d1fafd9764
Tyrantz <raymanv3@gmail.com>
2016-04-18 04:52:46 +0000

    dummy generations counter

--- src/App.js
+++ src/App.js
@@ -61,6 +61,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
+        <h2>Generations: 0</h2>
         <Board size={this.state.size} running={this.state.running} speed={this.state.speed} />
         <button onClick={this.handleClick}>small</button>
         <button onClick={this.handleClick}>medium</button>
